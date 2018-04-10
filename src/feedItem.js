5a33771ee3bc0f763dd5df17fd3a8b640826105e
UnnamedHero <hero.nospam@gmail.com>
2018-04-10 23:06:09 +0300

    add styles and some refactoring

--- src/feedItem.js
+++ src/feedItem.js
@@ -5,41 +5,68 @@ const getNodeLink = node => getNodeTagValue(node, 'link');
 const getNodeTitle = node => getNodeTagValue(node, 'title');
 const getNodeDesc = node => getNodeTagValue(node, 'description');
 
-export default class FeedItem {
-  template = `<div class="feedTitle"></div>
-    <div class="feedDesc"></div>
-    <ul class="itemList"></ul>`;
+const makeFeedObject = (xml) => {
+  const feedObj = {
+    title: getNodeTitle(xml),
+    description: getNodeDesc(xml),
+  };
+  const feedItems = [...xml.getElementsByTagName('item')];
+  feedObj.items = feedItems.map(item => ({
+    title: getNodeTitle(item),
+    link: getNodeLink(item),
+  }));
+  return feedObj;
+};
+
+const makeFeedItemNode = (itemObj) => {
+  const elem = document.createElement('li');
+  const link = document.createElement('a');
+  link.setAttribute('href', itemObj.link);
+  link.textContent = itemObj.title;
+  elem.appendChild(link);
+  return elem;
+};
+
+const progressBar = `<div class="progress">
+<div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%"></div>
+</div>`;
+
+
+export default class Feed {
+  template = `
+  <h5 class="card-header text-white bg-secondary"></h5>
+  <div class="card-body">
+    <h5 class="card-title bg-light"></h5>
+    <ul class="list-group"></ul>
+  </div>`;
   constructor(url, parent) {
     this.url = url;
     this.parent = parent;
   }
-
   init() {
     const feedDiv = document.createElement('div');
-    feedDiv.classList.add('feedItem');
+    feedDiv.classList.add('card', 'w-75');
     feedDiv.innerHTML = this.template;
     this.parent.appendChild(feedDiv);
-    const title = feedDiv.querySelector('.feedTitle');
-    const desc = feedDiv.querySelector('.feedDesc');
-    const items = feedDiv.querySelector('.itemList');
+    const feedTitleNode = feedDiv.querySelector('.card-header');
+    const feedDescNode = feedDiv.querySelector('.card-title');
+    const feedItemsNode = feedDiv.querySelector('.list-group');
+    feedDescNode.innerHTML = progressBar;
     axios.get(this.url)
       .then((response) => {
         const dp = new DOMParser();
         const feedXML = dp.parseFromString(response.data, 'application/xml');
-        title.textContent = getNodeTitle(feedXML);
-        desc.textContent = getNodeDesc(feedXML);
-        const feedItems = [...feedXML.getElementsByTagName('item')];
-        feedItems.forEach((item) => {
-          const elem = document.createElement('li');
-          const link = document.createElement('a');
-          link.setAttribute('href', getNodeLink(item));
-          link.textContent = getNodeTitle(item);
-          elem.appendChild(link);
-          items.appendChild(elem);
+        const feedObj = makeFeedObject(feedXML);
+        feedTitleNode.textContent = feedObj.title;
+        feedDescNode.textContent = feedObj.description;
+        feedObj.items.forEach((item) => {
+          const elem = makeFeedItemNode(item);
+          feedItemsNode.appendChild(elem);
         });
       })
       .catch((error) => {
-        title.textContent = `${this.url} is bad URL, ${error}`;
+        feedTitleNode.textContent = `Some error has happend with ${this.url}`;
+        feedDescNode.textContent = error;
       });
   }
 }
