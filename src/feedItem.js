8a6037a1b1cb916f1865f17f2e108c919fda6594
UnnamedHero <hero.nospam@gmail.com>
2018-04-11 18:04:51 +0300

    add modal window

--- src/feedItem.js
+++ src/feedItem.js
@@ -1,10 +1,34 @@
-const makeFeedItemNode = (itemObj) => {
+const makeFeedItemNode = (itemObj, targetId) => {
   const elem = document.createElement('li');
-  const link = document.createElement('a');
-  link.setAttribute('href', itemObj.link);
-  link.textContent = itemObj.title;
-  elem.appendChild(link);
+  elem.classList.add('list-group-item', 'd-flex');
+  const button = itemObj.description.length > 0 ?
+    `<button type="button" class="btn btn-info badge badge-primary badge-pill ml-auto p-2" data-toggle="modal" data-target="#${targetId}"> ? </button>` :
+    '';
+  const html = `<a href="${itemObj.link}">${itemObj.title}</a>${button}`;
+  elem.innerHTML = html;
   return elem;
+};
+
+const makeFeedItemModal = (itemObj, targetId) => {
+  const modalDiv = document.createElement('div');
+  modalDiv.innerHTML = `<div class="modal fade" id="${targetId}" tabindex="-1" role="dialog" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+        ${itemObj.description}
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        </div>
+      </div>
+    </div>
+  </div>`;
+  return modalDiv;
 };
 
 export default (parent, feedObj) => {
@@ -22,7 +46,12 @@ export default (parent, feedObj) => {
   feedTitleNode.textContent = feedObj.title;
   feedDescNode.textContent = feedObj.description;
   feedObj.items.forEach((item) => {
-    const elem = makeFeedItemNode(item);
+    const elemId = `target${Math.floor(Math.random() * 1e5)}"`;
+    const elem = makeFeedItemNode(item, elemId);
+    if (item.description.length > 0) {
+      const elemModal = makeFeedItemModal(item, elemId);
+      parent.appendChild(elemModal);
+    }
     feedItemsNode.appendChild(elem);
   });
   parent.appendChild(feedDiv);
